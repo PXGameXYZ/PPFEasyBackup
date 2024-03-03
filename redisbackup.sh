00599e83421ef7dfd692b26e8061fefb1ba46224
d,0,0,64 <144173370+d0064@users.noreply.github.com>
2024-03-03 16:54:26 +0400

    Add the script for redis

---
+++ redisbackup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+DATE=$(date +"%Y%m%d%H%M%S")
+REDIS_BACKUP_DIR="$HOME/redis_backups"
+
+cp /var/lib/redis/dump.rdb "$REDIS_BACKUP_DIR/dump_$DATE.rdb"
+
+echo "Redis backup completed: $REDIS_BACKUP_DIR/dump_$DATE.rdb"
