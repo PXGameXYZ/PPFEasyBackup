a1071cb61a0744ac69bfa294d1fbdbe215c463b1
d,0,0,64 <144173370+d0064@users.noreply.github.com>
2024-03-03 19:36:07 +0400

    Update mysqlbackup.sh

--- mysqlbackup.sh
+++ mysqlbackup.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
 DATE=$(date +"%Y%m%d%H%M%S")
-MYSQL_BACKUP_DIR="$HOME/mysql_backups"
+MYSQL_BACKUP_DIR="/home/ubuntu/mysql_backups"
 MYSQL_USER="your_mysql_username"
 MYSQL_PASSWORD="your_mysql_password"
 DATABASES=("database1")
 
 mkdir -p "$MYSQL_BACKUP_DIR"
+MYSQL_CONFIG_FILE=$(mktemp)
+echo "[mysqldump]" > "$MYSQL_CONFIG_FILE"
+echo "user=$MYSQL_USER" >> "$MYSQL_CONFIG_FILE"
+echo "password=$MYSQL_PASSWORD" >> "$MYSQL_CONFIG_FILE"
 
 for DB_NAME in "${DATABASES[@]}"; do
-    mysqldump -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" "$DB_NAME" > "$MYSQL_BACKUP_DIR/$DB_NAME-$DATE.sql"
+    mysqldump --defaults-extra-file="$MYSQL_CONFIG_FILE" "$DB_NAME" > "$MYSQL_BACKUP_DIR/$DB_NAME-$DATE.sql"
 done
+
+rm "$MYSQL_CONFIG_FILE"
 
 echo "MySQL backup completed: $MYSQL_BACKUP_DIR"
