c2d62303edc0f0465f05ae9afcd9eaa4bb31998a
d,0,0,64 <144173370+d0064@users.noreply.github.com>
2024-03-03 19:35:48 +0400

    Update redisbackup.sh

--- redisbackup.sh
+++ redisbackup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DATE=$(date +"%Y%m%d%H%M%S")
-REDIS_BACKUP_DIR="/home/ubuntu/redis_backups"
+REDIS_BACKUP_DIR="/home/yourusername/redis_backups" # Replace with your username
 
 mkdir -p "$REDIS_BACKUP_DIR"
 cp /var/lib/redis/dump.rdb "$REDIS_BACKUP_DIR/dump_$DATE.rdb"
