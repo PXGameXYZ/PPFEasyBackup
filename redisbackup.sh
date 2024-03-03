9c178d556342fdb3f1c970920490f6e8c48682b2
d,0,0,64 <144173370+d0064@users.noreply.github.com>
2024-03-03 19:34:24 +0400

    Update redisbackup.sh

--- redisbackup.sh
+++ redisbackup.sh
@@ -3,6 +3,8 @@
 DATE=$(date +"%Y%m%d%H%M%S")
 REDIS_BACKUP_DIR="$HOME/redis_backups"
 
+mkdir -p "$REDIS_BACKUP_DIR"
+
 cp /var/lib/redis/dump.rdb "$REDIS_BACKUP_DIR/dump_$DATE.rdb"
 
 echo "Redis backup completed: $REDIS_BACKUP_DIR/dump_$DATE.rdb"
